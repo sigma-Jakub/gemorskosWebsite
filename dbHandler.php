270e169d1480bab49af1da4b427d36fb58616db6
Jakub <jakub.mazur@student.nhlstenden.com>
2026-01-12 20:37:16 +0100

    updated dbHandler

--- dbHandler.php
+++ dbHandler.php
@@ -2,7 +2,8 @@
     $dbHandler = null;
 
     try {
-        $dbHandler = new PDO("mysql:host=localhost;dbname=database;charset=utf8", "root", "qwerty");
+        $dbHandler = new PDO("mysql:host=mysql;dbname=database;charset=utf8", "root", "qwerty");
+        // CHANGE TJE HOST TO localhost TO MAKE IT WORK ON A SERVER (hopefully...)
     } catch(Exception $ex) {
         echo $ex;
     }
